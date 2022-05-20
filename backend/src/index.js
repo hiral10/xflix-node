0e372d81cc1ba2a91732fcba8b90515203e7f4f5
hiral10 <davehiral1011@gmail.com>
2022-05-20 14:59:14 +0530

    ME_BUILDOUT_XFLIX_NODE_MODULE_NODE  (Crio.Do)

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const app = require("./app");
 require('dotenv').config({path:"../.env"})
 mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true,useUnifiedTopology: true  })
-        .then(()=>console.log("connect to DB"))
+        .then(()=>console.log("connect to DB",process.env.MONGODB_URL))
         .catch((e)=>console.log("failed to connect to DB",e))
         
 app.listen(process.env.NODE_ENV,()=>{
-    console.log("Listening to port",8082)
+    console.log("Listening to port",process.env.NODE_ENV)
 })
