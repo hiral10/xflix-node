cbc3e8e84a915ccd1e4ece7ab302502ffb78966d
hiral10 <davehiral1011@gmail.com>
2022-10-12 10:35:07 +0530

    Added changes

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use("/v1", routes);
-app.use((req, res, next) => {
-    next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
-});
-app.use(errorHandler);
+// app.use((req, res, next) => {
+//     next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+// });
+// app.use(errorHandler);
 
 module.exports = app;
